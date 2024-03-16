fa0f1ff86e9fff61a87db5b80c38de797e5363dc
xProxyRed <xproxyred@gmail.com>
2024-03-16 20:39:19 +0100

    Update dellCache.py
    add versions check

--- dellCache.py
+++ dellCache.py
@@ -1,27 +1,43 @@
 import os
-import tkinter as tk
+import urllib.request
+import webbrowser
 from tkinter import messagebox
 
 local = os.getenv('LOCALAPPDATA')
 cache_path = local + '\\FiveM\\FiveM.app\\data\\cache'
+version = "1.0.0.2"
+remote_version = None
 
-def main(path):
-    def on_confirm():
-        messagebox.showinfo("Bestätigung", "Die Dateien wurden gelöscht.")
-        delete_files(path)
+def on_confirm():
+    messagebox.showinfo("Bestätigung", "Die Dateien wurden gelöscht.")
+    delete_files(cache_path)
 
-    def on_cancel():
-        messagebox.showinfo("Abgebrochen", "Das Löschen der Datei wurde abgebrochen.")
+def on_cancel():
+    messagebox.showinfo("Abgebrochen", "Das Löschen der Datei wurde abgebrochen.")
 
-    root = tk.Tk()
-    root.withdraw()
-    result = messagebox.askyesno("Bestätigung", "Sind Sie sicher, dass Sie alle Dateien löschen wollen? '{}'".format(path), parent=root)
-    root.destroy()
+def on_download():
+    webbrowser.open_new("https://github.com/xProxyRed/FiveM-Cache-Clear/releases")
 
-    if result:
-        on_confirm()
+def main():
+    global remote_version
+    remote_version = urllib.request.urlopen("https://raw.githubusercontent.com/xProxyRed/FiveM-Cache-Clear/main/version").read().decode("utf-8").strip()
+    if remote_version != version:
+        result = messagebox.askyesno("Aktualisierung", "Eine neue Version ({}) ist verfügbar. Möchtest du sie jetzt herunterladen?".format(remote_version))
+        if result:
+            on_download()
+        else:
+            result = messagebox.askyesno("Bestätigung", "Sind Sie sicher, dass Sie alle Dateien löschen wollen? '{}'".format(cache_path))
+            if result:
+                on_confirm()
+            else:
+                on_cancel()
     else:
-        on_cancel()
+        result = messagebox.askyesno("Bestätigung", "Sind Sie sicher, dass Sie alle Dateien löschen wollen? '{}'".format(cache_path))
+        if result:
+            on_confirm()
+        else:
+            on_cancel()
+
 
 def delete_files(path):
     for root, dirs, files in os.walk(path, topdown=False):
@@ -32,4 +48,4 @@ def delete_files(path):
             dir_path = os.path.join(root, dir)
             os.rmdir(dir_path)
 
-main(cache_path)
+main()
